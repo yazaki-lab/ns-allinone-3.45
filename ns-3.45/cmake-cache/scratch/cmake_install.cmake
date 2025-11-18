77c543865bca8f0fa585a0c850649a86b55148b8
上川雅弘 <k2210182@gl.cc.uec.ac.jp>
2025-11-18 10:38:41 +0900

    前コミットのつづき

--- ns-3.45/cmake-cache/scratch/cmake_install.cmake
+++ ns-3.45/cmake-cache/scratch/cmake_install.cmake
@@ -39,6 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
+  include("/Users/kamikawamasahiro/Desktop/ns-allinone-3.45/ns-3.45/cmake-cache/scratch/channelutilization/cmake_install.cmake")
   include("/Users/kamikawamasahiro/Desktop/ns-allinone-3.45/ns-3.45/cmake-cache/scratch/nested-subdir/cmake_install.cmake")
 
 endif()
