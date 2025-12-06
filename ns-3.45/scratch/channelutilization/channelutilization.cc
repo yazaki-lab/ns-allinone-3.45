2543c4f385d6836b6d9b423fa06178208c9060fa
上川雅弘 <k2210182@gl.cc.uec.ac.jp>
2025-12-06 17:20:14 +0900

    変更した(ビルド通らず)

--- ns-3.45/scratch/channelutilization/channelutilization.cc
+++ ns-3.45/scratch/channelutilization/channelutilization.cc
@@ -334,7 +334,7 @@ int main(int argc, char *argv[]) {
     
     // 衝突率の計算 (Collision Rate)
     // 定義: 干渉・SINRによる全ドロップ数 / 全物理送信回数
-    double collisionRate = (g_phyTxTotal > 0) ? (double)g_phyRxDropCollision / (g_phyTxTotal+(double)g_phyRxDropCollision) * 100.0 : 0.0;
+    double collisionRate = (g_phyTxTotal > 0) ? (double)g_phyRxDropCollision / g_phyTxTotal * 100.0 : 0.0;
     
     // スループット, 遅延, ロス率
     monitor->CheckForLostPackets();
